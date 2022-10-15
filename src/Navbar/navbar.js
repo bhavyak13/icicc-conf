6128f1a517e0da2c15d6618c0a88647cd2f5052a
Bhavya Kawatra <bhavyakawatra6@gmail.com>
2022-10-16 02:23:19 +0530

    navbar home link fixed

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -14,7 +14,7 @@ function CommonNavbar() {
         <Nav className="me-auto" style={{ display: 'flex', width: '100%', justifyContent: 'space-around', fontSize: '15px', padding: '20' }}>
           <Nav.Link>
             <NavLink className={style.links}
-              to="/icicc-conf" 
+              to="/" 
             >
               Home
             </NavLink>
