23aab9a24dbd406d7d717c850f6373e2fd906010
Amith0205 <amithharish019@gmail.com>
2022-11-16 18:43:10 +0530

    Dates updated

--- src/Date.js
+++ src/Date.js
@@ -9,31 +9,31 @@ const Date=()=>{
   <marquee  direction="up" className={style.marquee} style={{width:"30vw"}} >
 
   <p><b>Manuscript Submission Deadline:</b><br />
-  <span className={style.colorDate}>November 15th, 2022</span></p>
+  <span className={style.colorDate}>December 10th, 2022</span></p>
   <hr />
 <p>
 <b>Notification of first review:</b><br />
-<span className={style.colorDate}>November 30th, 2022</span>
+<span className={style.colorDate}>December 20th, 2022</span>
 <hr />
 </p>
 <p>
 <b>Submission of revised manuscript</b><br />
-<span className={style.colorDate}>December 15th, 2022</span>
+<span className={style.colorDate}>December 31st, 2022</span>
 <hr />
 </p>
 <p>
 <b>Notification of Acceptance / Rejection</b><br />
-<span className={style.colorDate}>December 30th, 2022</span>
+<span className={style.colorDate}>January 10th, 2023</span>
 <hr />
 </p>
 <p>
 <b>Final manuscript due</b><br />
-<span className={style.colorDate}>January  15th, 2023</span>
+<span className={style.colorDate}>January  20th, 2023</span>
 <hr />
 </p>
 <p>
 <b>Registration Deadline</b><br />
-<span className={style.colorDate}>January 25th, 2023</span>
+<span className={style.colorDate}>January 30th, 2023</span>
 <hr />
 </p>
 <p>
