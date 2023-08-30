8f439e99a1dc56c4c1d9e50e573e4dd5ef900c0c
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2023-08-30 13:54:39 +0530

    proposal templates

--- src/Papers/call_for_int_wshps.js
+++ src/Papers/call_for_int_wshps.js
@@ -47,7 +47,7 @@ Organizer and Convener<br />
 
 <b>ICICC-2024</b></p>
 
-{/* <Button variant="primary" href="">Workshop Proposal Template</Button> */}
+<Button variant="primary" > <a href={require("./Workshop_Proposal_Template.docx")} style={{color:"white",textDecoration:"none"}}>Workshop Proposal Template</a></Button>
   </div>
 );
 };
