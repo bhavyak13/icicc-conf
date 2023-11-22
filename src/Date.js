cc9f23407052d5d4623029f26ae1cd74f4b49e5b
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2023-11-22 20:27:48 +0530

    dates changed

--- src/Date.js
+++ src/Date.js
@@ -9,32 +9,32 @@ const Date=()=>{
   <marquee  direction="up" className={style.marquee} style={{width:"30vw"}} >
 
   <p><b>Paper Submission Deadline:</b><br />
-  <span className={style.colorDate}>November 20th, 2023</span></p>
+  <span className={style.colorDate}>December 15th, 2023</span></p>
   <hr />
 <p>
 <b>Notification of first review:</b><br />
-<span className={style.colorDate}>November 30th, 2023</span>
+<span className={style.colorDate}>December 20th, 2023</span>
 <hr />
 </p>
 <p>
 <b>Submission of revised manuscript</b><br />
-<span className={style.colorDate}>December 10th, 2023</span>
+<span className={style.colorDate}>December 25th, 2023</span>
 <hr />
 </p>
 <p>
 <b>Notification of Acceptance / Rejection</b><br />
-<span className={style.colorDate}>December 15th,2023
+<span className={style.colorDate}>December 30th,2023
 </span>
 <hr />
 </p>
 <p>
 <b>Final manuscript due</b><br />
-<span className={style.colorDate}>December 20th, 2023</span>
+<span className={style.colorDate}>December 30th, 2023</span>
 <hr />
 </p>
 <p>
 <b>Registration Deadline</b><br />
-<span className={style.colorDate}>December 20th, 2023</span>
+<span className={style.colorDate}>January 05th, 2024</span>
 <hr />
 </p>
 <p>
