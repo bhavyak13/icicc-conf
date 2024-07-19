8dfe944660a5b3c0eb513602aa9264e4b7ca6bc4
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2024-07-19 23:24:59 +0530

    links

--- src/PrevConf/icicc24.js
+++ src/PrevConf/icicc24.js
@@ -5,19 +5,36 @@ import style from "../header.module.css";
 const Icicc24=()=>{
     const links=[{
         title:"Volume 1",
-        anchor:"https://link.springer.com/book/10.1007/978-981-99-3315-0",
-        val:"https://link.springer.com/book/10.1007/978-981-99-3315-0"
+        anchor:"https://link.springer.com/book/9789819735877",
+        val:"https://link.springer.com/book/9789819735877"
     },
     {
         title:"Volume 2",
-        anchor:"https://link.springer.com/book/9789819940721",
-        val:"https://link.springer.com/book/9789819940721"
+        anchor:"https://link.springer.com/book/9789819735907",
+        val:"https://link.springer.com/book/9789819735907"
 
     },
     {
         title:"Volume 3",
-        anchor:"https://link.springer.com/book/9789819930098",
-        val:"https://link.springer.com/book/9789819930098"
+        anchor:"https://link.springer.com/book/9789819741519",
+        val:"https://link.springer.com/book/9789819741519"
+        
+    },
+    {
+        title:"Volume 4",
+        anchor:"https://link.springer.com/book/9789819738168",
+        val:"https://link.springer.com/book/9789819738168"
+    },
+    {
+        title:"Volume 5",
+        anchor:"https://link.springer.com/book/9789819741489",
+        val:"https://link.springer.com/book/9789819741489"
+
+    },
+    {
+        title:"Volume 6",
+        anchor:"https://link.springer.com/book/9789819742271",
+        val:"https://link.springer.com/book/9789819742271"        
     }
     ]
     const videos=[{
@@ -42,7 +59,7 @@ const Icicc24=()=>{
   <div className={style.flex}>
   <div className={style.abtInstCont}>
     {/* <p style={{fontSize:15,letterSpacing:1}}>Fifth version of International Conference in innovative Computing and Communication (ICICC-2023) was organized at Shaheed Sukhdev College of Business Studies in association with National Institute of Technology Patna, University of Valladolid Spain and Korea Institute of Digital Convergence South Korea on 19-20 February 2023 at New Delhi, India. ICICC-2023 received 864 papers from approximately 3600 authors and a total of 200 papers were accepted with an acceptance ratio of 23%. All accepted papers were published in Springer's Lecture Notes on Networks and Systems, an scopus indexed series. A total of 550 participants attended the conference including authors, keynotes, delegates, academicians, and industry experts. ICICC-2023 received papers from 30 countries. Due to unexpected COVID-19, ICICC-2023 was organized in hybrid mode.</p><br /> */}
-{/* <h3>LINK OF PROCEEDINGS OF ICICC 2024</h3><br />
+<h3>LINK OF PROCEEDINGS OF ICICC 2024</h3><br />
 <div className={style.volumes}>
 		<table class="table table-bordered table-hover" style={{textAlign:"left"}}>
             <tbody>
@@ -57,7 +74,7 @@ const Icicc24=()=>{
               
             </tbody>
           </table>
-</div> */}
+</div>
 
     <h3>VIDEOS OF ICICC 2024</h3><br />
 <div className={style.volumes}>
