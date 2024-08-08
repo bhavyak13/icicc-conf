acbb386b9dc97e97b0f717f6b65eecdf380c9b0b
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2024-08-08 22:50:45 +0530

    nit patna

--- src/Navbar/pageHeader.js
+++ src/Navbar/pageHeader.js
@@ -18,15 +18,12 @@ const Header = () => {
                 </div>
                 
                 <div>
-                    <h1 className={
-                        style.icicc
-                    }>8th INTERNATIONAL CONFERENCE ON INNOVATIVE COMPUTING AND COMMUNICATION<br/>
+                    <h1 style={{textAlign: "center", fontSize: "22px" ,color:'red',fontWeight:'600'}}>8th INTERNATIONAL CONFERENCE ON INNOVATIVE COMPUTING AND COMMUNICATION<br/>
                         (ICICC 2025)</h1>
                     <br/>
                     <h2 className={
                         style.organiser
-                    }>ORGANISED BY : SHAHEED SUKHDEV COLLEGE OF BUSINESS STUDIES, UNIVERSITY OF DELHI, NEW DELHI
-                    IN ASSOCIATION UNIVERSITY OF VALLADOLID SPAIN</h2>
+                    } style={{lineHeight:'25px' }}> ORGANISED BY: SHAHEED SUKHDEV COLLEGE OF BUSINESS STUDIES, UNIVERSITY OF DELHI, NEW DELHI <br/>IN ASSOCIATION WITH NATIONAL INSTITUTE OF TECHNOLOGY PATNA &<br/> UNIVERSITY OF VALLADOLID SPAIN</h2>
                     <h2 className={
                         style.dates
                     }>14-15th FEBRUARY 2025</h2>
@@ -40,20 +37,27 @@ const Header = () => {
                         style.headerLogo
                 }></img>
                 <img src={
+                        require('./images/logo2.jpeg')
+                    }
+                    className={
+                        style.headerLogo
+                }></img>
+                
+                </div>
+                <div className={style.logoCont}>
+                <img src={
                         require('./images/logospain.png')
                     }
                     className={
                         style.headerLogo
                 }></img>
-                </div>
-                <div className={style.logoCont}>
-                
                 <img src={
                         require('./images/Springer.png')
                     }
                     className={
                         style.headerLogo
                 }></img>
+                
                 </div>
 
                 
