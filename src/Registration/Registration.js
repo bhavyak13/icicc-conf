b850c265bb940cba780fcce85dbb6dc82878df1d
Amith0205 <89029802+Amith0205@users.noreply.github.com>
2024-11-08 19:06:37 +0530

    dates

--- src/Registration/Registration.js
+++ src/Registration/Registration.js
@@ -189,8 +189,8 @@ const Registration = () => {
           <thead>
             <tr>
               <th><b>Category</b></th>
-              <th><b>Early Registration (before 01st November, 2024) </b></th>
-              <th><b>Late Registration (after 01st November, 2024) </b></th>
+              <th><b>Early Registration (before 15th November, 2024) </b></th>
+              <th><b>Late Registration (after 15th November, 2024) </b></th>
             </tr>
           </thead>
 
@@ -254,8 +254,8 @@ const Registration = () => {
           <thead>
             <tr>
               <th><b>Category</b></th>
-              <th><b>Early Registration (before 01st November, 2024) </b></th>
-              <th><b>Late Registration (after 01st November, 2024) </b></th>
+              <th><b>Early Registration (before 15th November, 2024) </b></th>
+              <th><b>Late Registration (after 15th November, 2024) </b></th>
             </tr>
           </thead>
 
